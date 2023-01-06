bca45d0d473feee8119feace58afe7f32cdab799
Gong Xilong <122077814+GongXilong1@users.noreply.github.com>
2023-01-06 17:53:11 +0800

    Add files via upload

---
+++ Function/Function_01_快速体验函数.py
@@ -0,0 +1,3 @@
+import Circulate_10_九九乘法表
+
+Circulate_10_九九乘法表.multiple_table()
---
+++ Function/Function_09_体验模块.py
@@ -0,0 +1,5 @@
+import Function_09_分隔线模块
+
+Function_09_分隔线模块.print_line("-", 50)
+Function_09_分隔线模块.print_lines("*", 50)
+print(Function_09_分隔线模块.name)
