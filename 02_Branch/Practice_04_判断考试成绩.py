ff8ee6e692ee1bcaec1b92b3226e6bc3783af06c
Gong Xilong <122077814+GongXilong1@users.noreply.github.com>
2023-01-06 22:00:07 +0800

    Add files via upload

---
+++ 02_Branch/Practice_04_判断考试成绩.py
@@ -0,0 +1,10 @@
+# 定义两个变量 python_score / c_score，编写代码判断成绩
+
+python_score = 8
+c_score = 7
+
+# 要求只要有一门成绩 > 60 分数就算合格
+if python_score > 60 or c_score >60:
+    print("考试通过")
+else:
+    print("考试失败，继续努力")
