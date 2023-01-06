8d252c8d4ae4c54f16f8e892fa51e6b682f9b003
Gong Xilong <122077814+GongXilong1@users.noreply.github.com>
2023-01-06 22:03:58 +0800

    Add files via upload

---
+++ 04_Function/Function_02_第一个函数.py
@@ -0,0 +1,10 @@
+# 注意：定义好函数之后，只表示这个函数封装了一段代码
+# 如果不主动调用函数，函数是不会主动执行的
+
+def say_hello():
+    print("hello1")
+    print("hello2")
+    print("hello3")
+
+
+say_hello()  # 调用函数：在函数名后加（）
---
+++ 04_Function/Function_07_打印分隔线.py
@@ -0,0 +1,8 @@
+def print_line(char, times):  # 定义两个形参 
+
+    print(char * times)
+
+
+print_line("haha", 40)
+
+
