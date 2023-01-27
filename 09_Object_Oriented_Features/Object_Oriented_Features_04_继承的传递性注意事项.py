59ec2d178b4d68b26cc28c219bcc056badc47f67
Gong Xilong （宫喜龙） <122077814+GongXilong1@users.noreply.github.com>
2023-01-27 22:47:01 +0800

    Add files via upload

---
+++ 09_Object_Oriented_Features/Object_Oriented_Features_04_继承的传递性注意事项.py
@@ -0,0 +1,37 @@
+class Animal:
+    def eat(self):
+        print("吃---")
+
+    def drink(self):
+        print("喝---")
+
+    def run(self):
+        print("跑---")
+
+    def sleep(self):
+        print("睡---")
+
+
+class Dog(Animal):
+    def bark(self):
+        print("汪汪叫")
+
+
+class XiaoTianQuan(Dog):
+    def fly(self):
+        print("我会飞")
+
+
+class Cat(Animal):
+    def catch(self):
+        print("抓老鼠")
+
+
+# 创建一个哮天犬的对象
+xtq = XiaoTianQuan()
+
+xtq.fly()  # xtq调用XiaoTianQuan类中fly的方法
+xtq.bark()  # xtq调用Dog类中bark的方法
+xtq.eat()  # xtq调用Animal类中eat的方法
+# xtq.catch()  # XiaoTianQuan类和Cat类没有继承关系。
+
