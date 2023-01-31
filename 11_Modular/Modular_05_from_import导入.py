d398a4525a613fba28847808a4a3d115b509935f
Gong Xilong （宫喜龙） <122077814+GongXilong1@users.noreply.github.com>
2023-01-31 22:21:00 +0800

    Add files via upload

---
+++ 11_Modular/Modular_05_from_import导入.py
@@ -0,0 +1,8 @@
+from Modular_01_测试模块1 import Dog  # 从模块1中导入Dog类
+from Modular_02_测试模块2 import say_hello  # 从模块2中导入say_hello函数
+
+say_hello()  # 调用say_hello函数
+
+wangcai = Dog()  # 使用Dog类创建名字为wangcai的这个Dog对象
+print(wangcai)
+
